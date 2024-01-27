85c78a37d1a60690affca5e5b44e9c214eb8ba25
Juan Gajardo <jegajardog@gmail.com>
2024-01-26 22:33:29 -0300

    Manage get env variables internal pkg

---
+++ internal/env.go
@@ -0,0 +1,21 @@
+package internal
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+// use godot package to load/read the .env file and
+// return the value of the key
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
